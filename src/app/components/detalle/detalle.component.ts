0d8731b397a84ea9d18542432d1b273e7912af07
homeristico <homeristico@hotmail.com>
2023-03-14 22:50:19 -0500

    added Pie Charts to detail

--- src/app/components/detalle/detalle.component.ts
+++ src/app/components/detalle/detalle.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
+import { Chart } from "chart.js/auto";
 
 @Component({
   selector: 'app-detalle',
@@ -12,6 +13,57 @@ export class DetalleComponent implements OnInit {
 
   ngOnInit(): void {
     console.log(this.route.snapshot.paramMap.get('id'));
+
+    (async function() {
+      const data = [
+        { year: 2010, count: 10 },
+        { year: 2011, count: 20 },
+        { year: 2012, count: 15 },
+        { year: 2013, count: 25 },
+        { year: 2014, count: 22 },
+        { year: 2015, count: 30 },
+        { year: 2016, count: 28 },
+      ];
+
+      // GRAFICO DE BARRAS
+      // new Chart('myChart',
+      //   {
+      //     type: 'bar',
+      //     data: {
+      //       labels: data.map(row => row.year),
+      //       datasets: [
+      //         {
+      //           label: 'Acquisitions by year',
+      //           data: data.map(row => row.count)
+      //         }
+      //       ]
+      //     }
+      //   }
+      // );
+
+      new Chart('myChart',
+        {
+          type: 'pie',
+          data: {
+            labels: [
+              'Disponible',
+              'En Uso'
+            ],
+            datasets: [{
+              label: 'Hdd',
+              data: [32, 8],
+              backgroundColor: [
+                'rgb(54, 162, 235)',
+                'rgb(255, 205, 86)'
+              ],
+              hoverOffset: 4
+            }]
+          }
+        }
+      );
+    })();
   }
+
+
 
 }
